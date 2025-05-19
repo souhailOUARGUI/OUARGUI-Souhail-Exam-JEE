31eb6b4673b9ef79a97f6b5a0a9aea25e80ef45a
SouhailOUARGUI <ouargui550@gmail.com>
2025-05-19 10:45:48 +0100

    Created Exceptions

---
+++ src/main/java/com/example/ouarguisouhailexamjee/exceptions/ClientNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.ouarguisouhailexamjee.exceptions;
+
+public class ClientNotFoundException extends RuntimeException {
+    public ClientNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/ouarguisouhailexamjee/exceptions/CreditNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.ouarguisouhailexamjee.exceptions;
+
+public class CreditNotFoundException extends RuntimeException {
+    public CreditNotFoundException(String message) {
+        super(message);
+    }
+}
