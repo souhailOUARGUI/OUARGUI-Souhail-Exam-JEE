3d0eea4b53013747c3c08f105042886a055b7afa
SouhailOUARGUI <ouargui550@gmail.com>
2025-05-19 09:34:30 +0100

    Initial commit

---
+++ src/main/java/com/example/ouarguisouhailexamjee/OuarguiSouhailExamJeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ouarguisouhailexamjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuarguiSouhailExamJeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuarguiSouhailExamJeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ouarguisouhailexamjee/OuarguiSouhailExamJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ouarguisouhailexamjee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuarguiSouhailExamJeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
