7d46edef19afaf183052b5a3eb02a92fcca5a60a
SouhailOUARGUI <ouargui550@gmail.com>
2025-05-19 10:09:40 +0100

    test successful, created clients

--- src/main/java/com/example/ouarguisouhailexamjee/OuarguiSouhailExamJeeApplication.java
+++ src/main/java/com/example/ouarguisouhailexamjee/OuarguiSouhailExamJeeApplication.java
@@ -1,7 +1,15 @@
 package com.example.ouarguisouhailexamjee;
 
+import com.example.ouarguisouhailexamjee.entities.Client;
+import com.example.ouarguisouhailexamjee.repositories.ClientRepository;
+import com.example.ouarguisouhailexamjee.repositories.CreditRepository;
+import com.example.ouarguisouhailexamjee.repositories.RemboursementRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class OuarguiSouhailExamJeeApplication {
@@ -9,5 +17,23 @@ public class OuarguiSouhailExamJeeApplication {
     public static void main(String[] args) {
         SpringApplication.run(OuarguiSouhailExamJeeApplication.class, args);
     }
+
+@Bean
+    CommandLineRunner commandLineRunner(ClientRepository clientRepository,
+                                        CreditRepository creditRepository,
+                                        RemboursementRepository remboursementRepository
+                                        ) {
+        return args -> {
+                      Stream.of("souhail","Mohamed","fatima").forEach(name -> {
+              Client client = new Client();
+              client.setNom(name);
+              client.setEmail(name+"@gmail.com");
+              clientRepository.save(client);
+          });
+
+
+        };
+    }
+
 
 }
